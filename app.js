64322d667c2b3d6e2ceb9d82d4402fe006a2cdf2
srwojnar <szymonrwojnar@gmail.com>
2021-10-04 00:43:54 -0400

    Refactor code - pass 1

--- app.js
+++ app.js
@@ -1,33 +1,4 @@
-const statusDisplay = document.querySelector(".gameStatus");
-let gameActive = true;
-let player1 = null;
-let player2 = "computer";
-let currentPlayer = player1;
-let gameState = ["", "", "", "", "", "", "", "", ""];
-const winningMessage = () => `Player ${currentPlayer} has won!`;
-const drawMessage = () => "Game ended in a draw!";
-const currentPlayerTurn = () => {
-  if (player1 === null) {
-    return "";
-  }
-  return `It's ${currentPlayer}'s turn${
-    currentPlayer === "computer" ? "..." : ""
-  }`;
-};
-statusDisplay.innerHTML = currentPlayerTurn();
-function handleCellPlayed(clickedCell, clickedCellIndex) {
-  if (currentPlayer === null) {
-    currentPlayer = player1;
-  }
-  let playerSymbol = currentPlayer === player1 ? "X" : "O";
-  if (currentPlayer === player1) {
-    gameState[clickedCellIndex] = "X";
-  } else {
-    gameState[clickedCellIndex] = "O";
-  }
-  clickedCell.innerHTML = playerSymbol;
-}
-const winningConditions = [
+const WINNING_CONDITIONS = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -37,10 +8,62 @@ const winningConditions = [
   [0, 4, 8],
   [2, 4, 6]
 ];
+
+class Player {
+  constructor(name='Player') {
+    this.name = name;
+  }
+}
+
+class HumanPlayer extends Player {
+  constructor(name) {
+    super(name);
+    this.symbol = 'X';
+  }
+  isHuman() {
+    return true;
+  }
+}
+
+class ComputerPlayer extends Player {
+  constructor() {
+    super('Computer');
+    this.symbol = 'O';
+  }
+  isHuman() {
+    return false;
+  }
+}
+
+let isGameActive = false;
+let player1 = null;
+let player2 = null;
+let currentPlayer = null;
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+const statusDisplay = document.querySelector(".gameStatus");
+
+function showWhoIsPlaying() {
+  const players = document.getElementById('active-players');
+  players.innerHTML = `<span id="active-player-1">${player1.name}</span> vs <span id="active-player-2">${player2.name}</span>`;
+  players.classList.remove('hidden');
+}
+  
+const winningMessage = () => `Player ${currentPlayer.name} has won!`;
+const drawMessage = () => "Game ended in a draw!";
+const currentPlayerTurn = () => `It's ${currentPlayer.name}'s turn${currentPlayer.isHuman() ? '' : '...'}`;
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+  gameState[clickedCellIndex] = currentPlayer.symbol;
+  clickedCell.innerHTML = currentPlayer.symbol;
+  // Check if there is a winner or a draw
+  handleResultValidation()
+}
+
 function handleResultValidation() {
   let roundWon = false;
   for (let i = 0; i <= 7; i++) {
-    const winCondition = winningConditions[i];
+    const winCondition = WINNING_CONDITIONS[i];
     let a = gameState[winCondition[0]];
     let b = gameState[winCondition[1]];
     let c = gameState[winCondition[2]];
@@ -54,78 +77,122 @@ function handleResultValidation() {
   }
   if (roundWon) {
     statusDisplay.innerHTML = winningMessage();
-    gameActive = false;
+    isGameActive = false;
+    document.getElementById('restart-button').classList.remove('hidden');
     return;
   }
   let roundDraw = !gameState.includes("");
   if (roundDraw) {
     statusDisplay.innerHTML = drawMessage();
-    gameActive = false;
+    isGameActive = false;
+    document.getElementById('restart-button').classList.remove('hidden');
     return;
   }
-  handlePlayerChange();
 }
-function makeComputerMove() {
-  if (emptyCell) {
-    gameState[emptyCell] = "O";
-  }
-}
+
 function handlePlayerChange() {
   currentPlayer = currentPlayer === player1 ? player2 : player1;
 
-  if (currentPlayer === "computer") {
+  // If current player is not human, complete move automatically
+  if (!currentPlayer.isHuman()) {
     setTimeout(() => {
       let index = gameState.indexOf("");
       let cell = document.querySelector(`[data-cell-index="${index}"]`);
       handleCellPlayed(cell, index);
 
-      currentPlayer = player1;
-      statusDisplay.innerHTML = currentPlayerTurn();
+      // Give turn to other player if game is still active
+      if (isGameActive) {
+        currentPlayer = player1;
+        statusDisplay.innerHTML = currentPlayerTurn();
+      }
       return;
-    }, 4000);
+    }, 2000);
   }
   statusDisplay.innerHTML = currentPlayerTurn();
 }
 
 function handleCellClick(clickedCellEvent) {
-  if (currentPlayer === "computer") {
+  // If currentPlayer is not human, then we must wait for our turn
+  if (!currentPlayer.isHuman()) {
     return;
   }
+
   const clickedCell = clickedCellEvent.target;
   const clickedCellIndex = parseInt(
     clickedCell.getAttribute("data-cell-index")
   );
-  if (gameState[clickedCellIndex] !== "" || !gameActive) {
+  if (gameState[clickedCellIndex] !== "" || !isGameActive) {
     return;
   }
   handleCellPlayed(clickedCell, clickedCellIndex);
-  handleResultValidation();
+  handlePlayerChange();
 }
-function handleRestartGame() {
-  gameActive = true;
-  currentPlayer = "X";
-  gameState = ["", "", "", "", "", "", "", "", ""];
-  statusDisplay.innerHTML = currentPlayerTurn();
-  document.querySelectorAll(".cell").forEach((cell) => (cell.innerHTML = ""));
-}
-function handlePlayer1(event) {
-  const player1Name = document.getElementById("player1").value;
-  player1 = player1Name;
-  statusDisplay.innerHTML = currentPlayerTurn();
-}
-function handlePlayer2(event) {
-  const player2Name = document.getElementById("player2").value;
-  player2 = player2Name;
-  statusDisplay.innerHTML = currentPlayerTurn();
-}
-document
-  .querySelectorAll(".cell")
-  .forEach((cell) => cell.addEventListener("click", handleCellClick));
-document.querySelector(".restart").addEventListener("click", handleRestartGame);
 
-document
-  .querySelector("#buttonPlayer1")
-  .addEventListener("click", handlePlayer1);
-document
-  .querySelector("#buttonPlayer2")
-  .addEventListener("click", handlePlayer2);
+function handleRestartGame() {
+  currentPlayer = player1;
+  gameState = ["", "", "", "", "", "", "", "", ""];
+  document.querySelectorAll(".cell").forEach((cell) => (cell.innerHTML = ""));
+  document.getElementById('restart-button').classList.add('hidden');
+  handleStartGame()
+}
+
+function handleResetGame() {
+  isGameActive = false;
+  currentPlayer = null;
+
+  document.getElementById('player1').value = '';
+  document.getElementById('player2').value = '';
+  gameState = ["", "", "", "", "", "", "", "", ""];
+  document.querySelectorAll(".cell").forEach((cell) => (cell.innerHTML = ""));
+
+  const button = document.getElementById('start-game-button')
+  button.removeEventListener('click', handleResetGame);
+  button.addEventListener('click', handleStartGame);
+  button.innerText = 'Start game';
+
+
+  // Hide active players
+  document.getElementById('active-players').classList.add('hidden');
+  // Hide board
+  document.getElementById('board').classList.add('hidden');
+  // Hide current player turn info
+  document.getElementById('info').classList.add('hidden');
+}
+
+function handleStartGame() {
+  isGameActive = true;
+  gameState = ["", "", "", "", "", "", "", "", ""];
+  document.querySelectorAll(".cell").forEach((cell) => (cell.innerHTML = ""));
+  
+  player1 = document.getElementById("player1").value === '' ? new HumanPlayer(): new HumanPlayer(player1Name);
+  player2 = document.getElementById("player2").value === '' ? new ComputerPlayer(): new HumanPlayer(player2Name);
+  currentPlayer = player1;
+  
+  showWhoIsPlaying()
+
+  // Show the board
+  document.getElementById('board').classList.remove('hidden');
+  // Show current player turn
+  document.getElementById('info').classList.remove('hidden');
+  // Hide play again button
+  document.getElementById('restart-button').classList.add('hidden');
+  
+  
+  // Change handler function on button and innter text
+  const button = document.getElementById('start-game-button')
+  button.removeEventListener('click', handleStartGame);
+  button.addEventListener('click', handleResetGame);
+  button.innerText = 'Change players'
+
+  statusDisplay.innerHTML = currentPlayerTurn();
+}
+
+function ready() {
+  document
+    .querySelectorAll(".cell")
+    .forEach((cell) => cell.addEventListener("click", handleCellClick));
+  document.getElementById("restart-button").addEventListener("click", handleRestartGame);
+  document.getElementById("start-game-button").addEventListener("click", handleStartGame);
+}
+
+document.addEventListener('DOMContentLoaded', ready)
